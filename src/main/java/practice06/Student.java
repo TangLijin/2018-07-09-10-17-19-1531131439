cb227dcff8cd42932e329bfb0d635c4afa55cd7f
tanglijin <1614468806@qq.com>
2019-07-11 20:20:59 +0800

    finish practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private String Class;
+
+    public Student(String name, int age, String aClass) {
+        super(name, age);
+        Class = aClass;
+    }
+
+    public String getTheClass() {
+        return Class;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+
+    @Override
+    public void introduce() {
+        super.introduce();
+        System.out.print("I am a Student. I am at Class " + Class + ".");
+    }
 }
+
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private String Class;
+
+    public Teacher(String name, int age, String aClass) {
+        super(name, age);
+        Class = aClass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String getTheClass() {
+        return Class;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+    @Override
+    public void introduce() {
+        super.introduce();
+        if(Class == null){
+            System.out.print("I am a Teacher. I teach No Class.");
+        }else{
+            System.out.print("I am a Teacher. I teach Class " + Class + ".");
+        }
+    }
 }
