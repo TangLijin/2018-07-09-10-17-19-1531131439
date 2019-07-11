04722018b1b410d3c6397c208d72c469a8a2df20
tanglijin <1614468806@qq.com>
2019-07-11 20:45:41 +0800

    finish practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    private String number;
+
+    public Klass(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    @Override
+    public void introduce() {
+        super.introduce();
+        if(klass == null){
+            System.out.print("I am a Teacher. I teach No Class.");
+        }else{
+            System.out.print("I am a Teacher. I teach Class " + klass.getNumber() + ".");
+        }
+    }
+
+    public void introduceWith(Student student){
+        super.introduce();
+        if(student.getKlass() == this.klass){
+            System.out.print("I am a Teacher. I teach " + student.getName() + ".");
+        }else{
+            System.out.print("I am a Teacher. I don't teach " + student.getName() + ".");
+        }
+    }
 }
