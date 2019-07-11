b3302115511e7040c53fb9a7f6fa09eb641d9b47
tanglijin <1614468806@qq.com>
2019-07-11 19:57:16 +0800

    finish practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,23 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private String Class;
+
+    public Student(String name, int age, String aClass) {
+        super(name, age);
+        Class = aClass;
+    }
+
+    public String getTheClass() {
+        return Class;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("I am a Student. I am at Class " + Class + ".");
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,12 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("I am a Worker. I have a job.");
+    }
 }
