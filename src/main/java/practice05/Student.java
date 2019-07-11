78986bb8cd89ad0e2556df0e7233312bb3a23741
tanglijin <1614468806@qq.com>
2019-07-11 20:08:10 +0800

    finish practice05,but I think the practice04 and Practice05 mean the same

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private String Class;
+
+    public Student(String name, int age, String aClass) {
+        super(name, age);
+        Class = aClass;
+    }
+
+    public String getTheClass() {
+        return Class;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+
+    @Override
+    public void introduce() {
+        super.introduce();
+        System.out.print("I am a Student. I am at Class " + Class + ".");
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,13 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void introduce() {
+        super.introduce();
+        System.out.print("I am a Worker. I have a job.");
+    }
 }
